c47a5f7131623aae148c99761e68fbc2b97f124d
Hadi Alk <Hade.Kary24@gmail.com>
2024-03-09 20:20:26 +0200

    fix OAuth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -8,33 +8,29 @@ import { useNavigate } from 'react-router-dom';
 export default function OAuth() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const handleGoogleClick = async () => {
-        try
-        {
-            const provider = new GoogleAuthProvider();
-            const auth = getAuth(app);
-      
-            const result = await signInWithPopup(auth, provider);
-      
-            const res = await fetch('/api/auth/google', {
+    const handleGoogleClick = async () =>{
+      const provider = new GoogleAuthProvider()
+      provider.setCustomParameters({ prompt: 'select_account' })
+      try {
+          const resultsFromGoogle = await signInWithPopup(auth, provider)
+          const res = await fetch('/api/auth/google', {
               method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
+              headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
-                name: result.user.displayName,
-                email: result.user.email,
-                photo: result.user.photoURL,
+                  name: resultsFromGoogle.user.displayName,
+                  email: resultsFromGoogle.user.email,
+                  googlePhotoUrl: resultsFromGoogle.user.photoURL,
               }),
-            });
-            const data = await res.json();
-            dispatch(signInSuccess(data));
-            navigate('/');
-
-        }catch
-        {
-            console.log('Cloud not sign in with Google', error);
-        }
+              })
+          const data = await res.json()
+          if (res.ok){
+              dispatch(signInSuccess(data))
+              navigate('/')
+          }
+      } catch (error) {
+          console.log(error);
+      }
+      
     }
   return (
     <button type='button' onClick={handleGoogleClick} className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'> Continue with Google</button>
